2af3d6acf1e413b0ae96f1e397675de0a4b98743
peterson <petersondaronch@gmail.com>
2024-01-14 13:57:38 -0300

    allow customs and spells at git (actually allowed everything)

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue debugger",
+    }
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end,
+      "Start or continue python test debugger"
+    }
+  }
+}
+
+return M
